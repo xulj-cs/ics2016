9c5784cbb2964204a00a6cea3aca19830916710f
tracer-ics2016 <tracer@njuics.org>
2016-09-25 09:09:41 +0000

    >  compile NEMU 151220135 xlj Linux d5224b5e74cd 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  09:09:41 up  1:48,  2 users,  load average: 0.00, 0.00, 0.00 98b9ecb342c435b74f20916963f2b8fc9708fc1b

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -7,21 +7,22 @@ static void do_execute()
 	DATA_TYPE result;
 	result = op_src->val - op_dest->val;
 //	Log("%d%d",op_dest->val,op_src->val);
+
 	cpu.OF=concat(OF_,SUFFIX)(op_src->val,op_dest->val,'-');
 	cpu.SF=MSB(result);
 	cpu.ZF=!result;
 	cpu.PF=concat(PF_,SUFFIX)(result);
 	cpu.CF=concat(CF_,SUFFIX)(op_src->val,op_dest->val,'-');
 	print_asm_template2();
+
 }
 make_helper(concat(cmp_ib2rm_,SUFFIX))
 {
 	int len = concat(decode_rm_,SUFFIX)(eip +1);
-	
 	op_dest->type=OP_TYPE_IMM;
 	op_dest->imm=instr_fetch(eip+1+len,1);
 	op_dest->val = op_dest->imm;
-	
+	snprintf(op_dest->str, OP_STR_SIZE, "$0x%x", op_dest->imm);	
 	len +=2;
 	
 	do_execute();
