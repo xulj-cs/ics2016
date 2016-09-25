08f706687f98a4c4cca596dfb1c7a4fc41cf74d3
tracer-ics2016 <tracer@njuics.org>
2016-09-25 09:18:19 +0000

    >  compile NEMU 151220135 xlj Linux d5224b5e74cd 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  09:18:19 up  1:57,  2 users,  load average: 0.00, 0.00, 0.00 1900ee227e655a921a2c185b8fb0dc35878e10d

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -1,33 +1,39 @@
 #include "cpu/exec/template-start.h"
+#include "cpu/decode/modrm.h"
 #define instr cmp
 
 #if DATA_BYTE == 2 ||DATA_BYTE == 4 
 static void do_execute()
 {
 	DATA_TYPE result;
-	result = op_src->val - op_dest->val;
+	result = op_dest->val - op_src->val;
 //	Log("%d%d",op_dest->val,op_src->val);
 
-	cpu.OF=concat(OF_,SUFFIX)(op_src->val,op_dest->val,'-');
+	cpu.OF=concat(OF_,SUFFIX)(op_dest->val,op_src->val,'-');
 	cpu.SF=MSB(result);
 	cpu.ZF=!result;
 	cpu.PF=concat(PF_,SUFFIX)(result);
-	cpu.CF=concat(CF_,SUFFIX)(op_src->val,op_dest->val,'-');
+	cpu.CF=concat(CF_,SUFFIX)(op_dest->val,op_src->val,'-');
 	print_asm_template2();
 
 }
 make_helper(concat(cmp_ib2rm_,SUFFIX))
 {
-	int len = concat(decode_rm_,SUFFIX)(eip +1);
+	/*int len = concat(decode_rm_,SUFFIX)(eip +1);
 	op_dest->type=OP_TYPE_IMM;
 	op_dest->imm=instr_fetch(eip+1+len,1);
 	op_dest->val = op_dest->imm;
 	snprintf(op_dest->str, OP_STR_SIZE, "$0x%x", op_dest->imm);	
 	len +=2;
-	
+	*/
+	op_dest->size = DATA_BYTE;
+	int len =read_ModR_M( eip+1 ,op_dest,op_src2);
+//	snprintf(op_src2->str, OP_STR_SIZE, "%%%s", REG_NAME(reg->reg));
+
+	len += concat(decode_i_,SUFFIX)(eip+1+len);
 	do_execute();
 
-	return len;
+	return len+1;
 }
 
 #endif
