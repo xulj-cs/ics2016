6bf6ef41a3c9e84625234f9da8336879800c5cc9
tracer-ics2016 <tracer@njuics.org>
2016-09-27 08:19:28 +0000

    >  compile NEMU 151220135 xlj Linux d5224b5e74cd 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  08:19:28 up 7 min,  2 users,  load average: 0.22, 0.24, 0.17 2d628ffc93cd428b3bc811b6e270b006fe2b575d

--- nemu/src/cpu/exec/eflags/eflags-template.h
+++ nemu/src/cpu/exec/eflags/eflags-template.h
@@ -5,15 +5,15 @@ int concat(CF_,SUFFIX)(DATA_TYPE x,DATA_TYPE y,char op)
 	if(op == '+')
 	{
 		DATA_TYPE result = x+y;
-		if(result<x)
+		if(result<x)		//result=x+y-2^n <==> result < x <==> x+y>=2^n
 			return 1;
 		else
 			return 0;
 	}
 	else
 	{
-		DATA_TYPE result = x-y;
-		if(result>x)
+	//	DATA_TYPE result = x-y;
+		if( x < y )			//result=x-y+2^n <==> result > x <==> x < y
 			return 1;
 		else
 			return 0;
@@ -39,15 +39,21 @@ int concat(OF_,SUFFIX)(DATA_TYPE_S x,DATA_TYPE_S y,char op)
 {
 	DATA_TYPE_S result;
 	if(op == '+')
+	{	
 		result = x+y;
+		if ( MSB(x)==MSB(y) && MSB(x)!=MSB(result) )
+			return 1;
+		else
+			return 0;
+	}
 	else
+	{	
 		result = x-y;
-	
-	if( MSB(x)==MSB(y) && MSB(x)!=MSB(result) )
-		return 1;
-	else
-		return 0;
-
+		if( MSB(x)!=MSB(y) && MSB(x)!=MSB(result) )
+			return 1;
+		else
+			return 0;
+	}
 }
 
 #include "cpu/exec/template-end.h"
