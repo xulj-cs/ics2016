3ca7f61c12139c6226b4bc60ca04758d9d1fb54d
tracer-ics2016 <tracer@njuics.org>
2016-09-28 08:55:44 +0000

    >  compile NEMU 151220135 xlj Linux d5224b5e74cd 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  08:55:44 up 51 min,  1 user,  load average: 0.00, 0.01, 0.00 998993aacb6538cc15dce0d6a2aee5e67677cf19

--- nemu/src/cpu/exec/transfer/jmp-template.h
+++ nemu/src/cpu/exec/transfer/jmp-template.h
@@ -9,8 +9,10 @@ static void do_execute(){
 
 	if(DATA_BYTE == 2)
 		cpu.eip = cpu.eip & 0x0000ffff;
-
-	print_asm_template1();
+	if(DATA_BYTE == 1)
+		print_asm(str(instr) " %x",cpu.eip+1+1);
+	else
+		print_asm(str(instr) " %x",cpu.eip+1+4);
 }
 
 make_instr_helper(i)
