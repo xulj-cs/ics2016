86c0f92fee11fe181b94f8623f90f03763716727
tracer-ics2016 <tracer@njuics.org>
2016-09-28 08:44:52 +0000

    >  compile NEMU 151220135 xlj Linux d5224b5e74cd 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  08:44:52 up 40 min,  1 user,  load average: 0.00, 0.00, 0.00 97f55052d342ab6159b795f33c3f944e596895eb

--- nemu/src/cpu/exec/transfer/call-template.h
+++ nemu/src/cpu/exec/transfer/call-template.h
@@ -16,7 +16,7 @@ static void do_execute(){
 //	print_asm_template1();
 //	print_asm(str(instr) " %s",cpu.eip + 1 + DATA_BYTE)
 
-	printf("%s\t%x\n",str(cpu.eip),cpu.eip+1+DATA_BYTE);
+	printf("%s\t%x\n",str(instr),cpu.eip+1+DATA_BYTE);
 
 }
 //#if DATA_BYTE == 2 || DATA_BYTE == 4
