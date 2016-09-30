6adde840b3d8ddad2ecb26adcd4e502fc9685265
tracer-ics2016 <tracer@njuics.org>
2016-09-30 15:31:20 +0000

    >  compile NEMU 151220135 xlj Linux d5224b5e74cd 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  15:31:20 up 54 min,  4 users,  load average: 0.00, 0.00, 0.00 d1a24649339eb7eb5ef48a49849fb4b04916cd46

--- nemu/src/cpu/exec/logic/or-template.h
+++ nemu/src/cpu/exec/logic/or-template.h
@@ -7,8 +7,11 @@ static void do_execute () {
 	OPERAND_W(op_dest, result);
 
 	/* TODO: Update EFLAGS. */
-	panic("please implement me");
-
+	//	panic("please implement me");
+	cpu.CF = cpu.OF =0;
+	cpu.ZF = !result;
+	cpu.SF = MSB(result);
+	cpu.PF = concat(PF_,SUFFIX)(result);
 	print_asm_template2();
 }
 
