1db8002914a7b504a706cf0427ceedd18d328b92
tracer-ics2016 <tracer@njuics.org>
2016-10-01 04:04:58 +0000

    >  compile NEMU 151220135 xlj Linux d5224b5e74cd 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  04:04:58 up  2:52,  5 users,  load average: 0.03, 0.03, 0.00 4f09d9de68427cab33224e5c2be212dd3e96ff62

--- nemu/src/cpu/exec/data-mov/push-template.h
+++ nemu/src/cpu/exec/data-mov/push-template.h
@@ -1,17 +1,20 @@
 #include "cpu/exec/template-start.h"
 #define instr push
 
-#if DATA_BYTE == 2 || DATA_BYTE == 4
 
 static void do_execute(){
+	if(op_src->type == OP_TYPE_IMM )
+		reg_l(R_ESP) -= 4;
 	reg_l(R_ESP) -= DATA_BYTE;
 	MEM_W(reg_l(R_ESP),op_src->val);
 
 	print_asm_template1();
 }
-//if DATA_BYTE == 2 || DATA_BYTE == 4
+#if DATA_BYTE == 2 || DATA_BYTE == 4
 
 make_instr_helper(r)
 make_instr_helper(rm)   //in fact only can be m
 #endif
+
+make_instr_helper(i)
 #include "cpu/exec/template-end.h"
