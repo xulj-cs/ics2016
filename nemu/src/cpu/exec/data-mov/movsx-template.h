fb44ebc9985f1aa67cf84226f14c88a994f4b423
tracer-ics2016 <tracer@njuics.org>
2016-10-01 02:37:15 +0000

    >  compile NEMU 151220135 xlj Linux d5224b5e74cd 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  02:37:15 up  1:24,  5 users,  load average: 0.00, 0.00, 0.00 16e3f84efca1411e7daf0097f5fd3b03d95fb9b

---
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -0,0 +1,75 @@
+#include "cpu/exec/template-start.h"
+
+#define instr movsx
+
+static void do_execute(){
+	OPERAND_W (op_dest,op_src->val);
+	print_asm_template2();
+}
+
+make_helper ( concat(movsx_srmb2r_,SUFFIX )){
+	
+	op_src->size = 1;
+	ModR_M m;
+	m.val = instr_fetch(eip+1,1);
+	op_dest->type = OP_TYPE_REG;
+	op_dest->reg = m.reg;
+	int len;
+	if(m.mod == 3){
+		op_src->type = OP_TYPE_REG;
+		op_src->reg = m.R_M;
+		op_src->val = (int8_t)reg_b(m.R_M);
+
+		len = 1;
+	}
+
+	else
+	{
+		len = load_addr( eip+1, &m , op_src);
+		op_src -> val = (int8_t)swaddr_read(op_src->addr,op_src->size);
+		
+	}
+	
+	do_execute();
+
+	return 1+len; 
+}
+
+#if DATA_BYTE == 4
+make_helper(movsx_srmw2r_l){
+	
+	op_src->size = 2;
+	ModR_M m;
+	m.val = instr_fetch(eip+1,1);
+	op_dest->type = OP_TYPE_REG;
+	op_dest->reg = m.reg;
+	int len;
+	if(m.mod == 3){
+		op_src->type = OP_TYPE_REG;
+		op_src->reg = m.R_M;
+		op_src->val = (int8_t)reg_w(m.R_M);
+
+		len = 1;
+	}
+
+	else
+	{
+		len = load_addr( eip+1, &m , op_src);
+		op_src -> val = (int8_t)swaddr_read(op_src->addr,op_src->size);
+		
+	}
+	
+	do_execute();
+
+	return 1+len; 
+	
+	
+}
+#endif
+
+#include "cpu/exec/template-end.h"
+
+
+
+
+
---
+++ nemu/src/cpu/exec/data-mov/movsx.h
@@ -0,0 +1,8 @@
+#ifndef __MOVSX_H__
+#define __MOVSX_H__
+
+make_helper(movsx_srmb2r_v);
+
+make_helper(movsx_srmw2r_l);
+
+#endif
