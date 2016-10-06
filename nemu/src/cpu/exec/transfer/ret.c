5351d1ae8026b342ef0dee72fb59162d7f0fe53f
tracer-ics2016 <tracer@njuics.org>
2016-10-06 15:15:40 +0000

    >  compile NEMU 151220135 xlj Linux d5224b5e74cd 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  15:15:40 up  1:11,  6 users,  load average: 0.00, 0.00, 0.00 cfbbd1cb5afae52531661f9ab34f12f7d709fa4

--- nemu/src/cpu/exec/transfer/ret.c
+++ nemu/src/cpu/exec/transfer/ret.c
@@ -12,6 +12,19 @@ make_helper(ret_near_w){
 	print_asm(str(ret));
 	return 0;
 }
+
+make_helper(ret_near_i_w){
+	//ret_near_w(eip);
+	cpu.ip=MEM_R(reg_l(R_ESP));
+	cpu.eip = cpu.eip & 0x0000ffff;
+	reg_l(R_ESP) += DATA_BYTE;
+	
+	decode_i_w(eip+1);
+	reg_l(R_ESP) += op_src->val;
+	
+	print_asm(str(ret) " %x",op_src->val);
+	return 0;
+}
 #undef DATA_BYTE
 
 
@@ -24,20 +37,22 @@ make_helper(ret_near_l){
 	print_asm(str(ret));
 	return 0;
 }
-#undef DATA_BYTE
-
-make_helper(ret_near_i_w){
-	reg_l(R_ESP) += decode_i_w(eip+1);
-	ret_near_w(eip);
-	return 0;
-}
 
 make_helper(ret_near_i_l){
-	reg_l(R_ESP) += decode_i_w(eip+1);
-	ret_near_l(eip);
+	//	ret_near_l(eip);
+	cpu.eip=MEM_R(reg_l(R_ESP));
+	
+	reg_l(R_ESP) += DATA_BYTE;
+
+	decode_i_w(eip+1);
+	reg_l(R_ESP) +=  op_src->val;
+
+	print_asm(str(ret) " %x",op_src->val);
+
 	return 0;
 }
 
+#undef DATA_BYTE
 
 
 make_helper_v(ret_near)
