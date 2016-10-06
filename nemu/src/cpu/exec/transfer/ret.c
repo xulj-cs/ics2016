4085c05d9270d7263c32183f1990cb59045bc4b6
tracer-ics2016 <tracer@njuics.org>
2016-10-06 15:05:26 +0000

    >  compile NEMU 151220135 xlj Linux d5224b5e74cd 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  15:05:26 up  1:01,  6 users,  load average: 0.00, 0.00, 0.00 895b91bb24dedf037826c7cebcd35b8b64bda123

--- nemu/src/cpu/exec/transfer/ret.c
+++ nemu/src/cpu/exec/transfer/ret.c
@@ -5,17 +5,18 @@
 #define MEM_R(addr) swaddr_read(addr,DATA_BYTE)
 
 #define DATA_BYTE 2
-make_helper(ret_w){
+make_helper(ret_near_w){
 	cpu.ip=MEM_R(reg_l(R_ESP));
-	eip = eip & 0x0000ffff;
+	cpu.eip = cpu.eip & 0x0000ffff;
 	reg_l(R_ESP) += DATA_BYTE;
 	print_asm(str(ret));
 	return 0;
 }
 #undef DATA_BYTE
 
+
 #define DATA_BYTE 4
-make_helper(ret_l){
+make_helper(ret_near_l){
 	cpu.eip=MEM_R(reg_l(R_ESP));
 	
 	reg_l(R_ESP) += DATA_BYTE;
@@ -25,7 +26,19 @@ make_helper(ret_l){
 }
 #undef DATA_BYTE
 
+make_helper(ret_near_i_w){
+	reg_l(R_ESP) += decode_i_w(eip+1);
+	ret_near_w(eip);
+	return 0;
+}
+
+make_helper(ret_near_i_l){
+	reg_l(R_ESP) += decode_i_w(eip+1);
+	ret_near_l(eip);
+	return 0;
+}
 
 
-make_helper_v(ret)
 
+make_helper_v(ret_near)
+make_helper_v(ret_near_i)
--- nemu/src/cpu/exec/transfer/ret.h
+++ nemu/src/cpu/exec/transfer/ret.h
@@ -1,8 +1,9 @@
 #ifndef __RET_H__
 #define __RET_H__
-make_helper(ret_w);
-make_helper(ret_l);
+//make_helper(ret_w);
+//make_helper(ret_l);
 
-make_helper(ret_v);
+make_helper(ret_near_v);
+make_helper(ret_near_i_v);
 
 #endif
