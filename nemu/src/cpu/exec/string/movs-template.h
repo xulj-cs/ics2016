ad2c13aa11aca34e9f04eaeeb920180104f4e507
tracer-ics2016 <tracer@njuics.org>
2016-10-06 14:43:12 +0000

    >  compile NEMU 151220135 xlj Linux d5224b5e74cd 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  14:43:12 up 38 min,  5 users,  load average: 0.08, 0.02, 0.01 ff4604d9df4b9ed6d0a8e9411f1d612782786155

---
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -0,0 +1,18 @@
+#include "cpu/exec/template-start.h"
+
+
+
+make_helper(concat(movs_,SUFFIX)){
+	MEM_W( reg_l(R_EDI) , MEM_R( reg_l(R_ESI) ) );
+	if(!cpu.DF){
+		reg_l(R_EDI)+=DATA_BYTE;
+		reg_l(R_ESI)+=DATA_BYTE;
+	}
+	else{
+		reg_l(R_EDI)-=DATA_BYTE;
+		reg_l(R_ESI)-=DATA_BYTE;
+	}
+	return 1;
+}
+
+#include "cpu/exec/template-end.h"
