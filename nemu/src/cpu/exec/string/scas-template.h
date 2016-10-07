60ab06b90f39aaa06afb99b998b7fde66cccca97
tracer-ics2016 <tracer@njuics.org>
2016-10-07 04:26:41 +0000

    >  compile NEMU 151220135 xlj Linux d5224b5e74cd 4.4.17-boot2docker #1 SMP Mon Aug 15 17:12:38 UTC 2016 x86_64 GNU/Linux  04:26:41 up  3:04,  4 users,  load average: 0.05, 0.01, 0.00 d6ae31b7247b48e5eca63e4f1ca5c6b2881c7da1

--- nemu/src/cpu/exec/string/scas-template.h
+++ nemu/src/cpu/exec/string/scas-template.h
@@ -4,18 +4,20 @@
 
 make_helper(concat(scas_,SUFFIX)){
 	DATA_TYPE  result = REG(R_EAX) -  MEM_R( reg_l(R_EDI) ) ;
-	if(!cpu.DF){
-		reg_l(R_EDI)+=DATA_BYTE;
-	}
-	else{
-		reg_l(R_EDI)-=DATA_BYTE;
-	}
 	
 	cpu.OF=concat(OF_,SUFFIX)( REG(R_EAX),MEM_R( reg_l(R_EDI) ),'-' );
 	cpu.SF=MSB(result);
 	cpu.ZF=!result;
 	cpu.PF=concat(PF_,SUFFIX)(result);
 	cpu.CF=concat(CF_,SUFFIX)( REG(R_EAX),MEM_R( reg_l(R_EDI)), '-' );
+
+	if(!cpu.DF){
+		reg_l(R_EDI)+=DATA_BYTE;
+	}
+	else{
+		reg_l(R_EDI)-=DATA_BYTE;
+	}
+
 
 	print_asm(str(instr) str(SUFFIX) " %s%s","%es:(%edi),%",REG_NAME(R_EAX));
 //	print_asm(str(%ds:(%esi),%es:(%edi)));
