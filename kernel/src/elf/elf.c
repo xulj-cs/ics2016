3a30925a7cd083ae353a7031afd1cce2c5dbe4bc
tracer-ics2016 <tracer@njuics.org>
2016-10-20 03:21:07 +0800

    >  compile NEMU 151220135 xulijun Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.36-1+deb8u1 (2016-09-03) i686 GNU/Linux  03:21:07 up  8:28,  1 user,  load average: 0.08, 0.03, 0.05 c52b044ebac2d9619486979043f6ee48103880ac

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -31,24 +31,31 @@ uint32_t loader() {
 	elf = (void*)buf;
 
 	/* TODO: fix the magic number with the correct one */
-	const uint32_t elf_magic = 0x464c4571;
+	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
-
+	
+	//elf->e_phoff;
+	//elf->e_phentsize;
+	//elf->e_phnum;
+	
 	/* Load each program segment */
-	panic("please implement me");
-	for(; true; ) {
+	//panic("please implement me");
+	ph = &buf[elf->e_phoff];
+	int i;
+	for(i=0;i<elf->e_phnum ;i++ ) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
 
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			 
+			 swaddr_write(ph->p_vaddr,buf[ph->p_offset],ph->p_filesz);
 			 
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
+			memset(ph->p_vaddr+ph->p_filesz,0,ph->p_memsz-ph->p_filesz);
 
 
 #ifdef IA32_PAGE
@@ -58,6 +65,7 @@ uint32_t loader() {
 			if(cur_brk < new_brk) { max_brk = cur_brk = new_brk; }
 #endif
 		}
+	ph=(uint8_t*)ph + elf->e_phentsize;
 	}
 
 	volatile uint32_t entry = elf->e_entry;
