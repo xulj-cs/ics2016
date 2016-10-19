1d61183b923f590f63db65efacda0d80665633dc
tracer-ics2016 <tracer@njuics.org>
2016-10-20 05:57:18 +0800

    >  compile kernel 151220135 xulijun Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.36-1+deb8u1 (2016-09-03) i686 GNU/Linux  05:57:18 up 11:04,  2 users,  load average: 0.00, 0.01, 0.05 cc90e83f18388510527256a44d22ac4f944f57e8

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -67,9 +67,9 @@ uint32_t loader() {
 //			strncpy((char*)p,(const char *)&buf[ph->p_offset],ph->p_filesz);					
 //			ramdisk_write(&buf[ph->p_offset],ph->p_vaddr,ph->p_filesz);
 //			nemu_assert(0);
-//		nemu_assert(ph->p_vaddr==0x800000);
-			memcpy((void *)ph->p_vaddr,&buf[ph->p_offset],ph->p_filesz);
-//			nemu_assert(0);
+//			nemu_assert(ph->p_vaddr==0x800000);
+			memcpy((void *)va_to_pa(ph->p_vaddr),&buf[ph->p_offset],ph->p_filesz);
+			nemu_assert(0);
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
