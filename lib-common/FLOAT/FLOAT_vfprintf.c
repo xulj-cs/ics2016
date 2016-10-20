97a98a24b6f1fe1270b4e026e43bed60a207ea11
tracer-ics2016 <tracer@njuics.org>
2016-10-20 19:41:41 +0800

    >  run 151220135 xulijun Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.36-1+deb8u1 (2016-09-03) i686 GNU/Linux  19:41:41 up 1 day, 49 min,  1 user,  load average: 0.00, 0.01, 0.05 38cbee73e74e586cdd09b28fe18634358f63c63

--- lib-common/FLOAT/FLOAT_vfprintf.c
+++ lib-common/FLOAT/FLOAT_vfprintf.c
@@ -49,6 +49,7 @@ __attribute__((used)) static int format_FLOAT(FILE *stream, FLOAT f) {
 	}
 //	printf("%d\n",len);
 	buf[len+6]='\0';
+//	printf("buf==%s",buf);
 	//int len = sprintf(buf, "0x%08x", f);
 	return __stdio_fwrite(buf, len+7, stream);
 }
