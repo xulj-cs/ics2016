3b69fe865f11c0e2e0bb3658b9dbfe37fdbc5784
tracer-ics2016 <tracer@njuics.org>
2016-10-22 03:38:32 +0800

    >  compile NEMU 151220135 xulijun Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.36-1+deb8u1 (2016-09-03) i686 GNU/Linux  03:38:32 up 1 day,  4:04,  2 users,  load average: 0.21, 0.10, 0.07 cd20c5d946a24c5aead3caa69f6fdc28c67f2102

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -1,8 +1,8 @@
 #include "common.h"
-#include "macro.h"
-
 #include <stdlib.h>
 #include <time.h>
+#include "macro.h"
+
 uint32_t dram_read(hwaddr_t , size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
 
