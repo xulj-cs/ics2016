c23b7f0e320561b59b52d959ed99e8ea46bf6185
tracer-ics2016 <tracer@njuics.org>
2016-10-21 18:26:06 +0800

    >  compile NEMU 151220135 xulijun Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.36-1+deb8u1 (2016-09-03) i686 GNU/Linux  18:26:06 up 18:52,  2 users,  load average: 0.08, 0.08, 0.10 5f818fcad001cadee8ca90293c18c3cb7e44c10

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -12,6 +12,7 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 #ifdef HAS_CACHE
 //	if(addr==0x801200)
 //		Log("%x",dram_read(addr,4));
+	Log("addr:%x,len:%x",addr,len);
 	Log("dram:%x,cache:%x",dram_read(addr,len),cache_read(addr,len));
 	return cache_read(addr, len) & (~0u >> ((4 - len) << 3));
 #else
