caed1125c4a0899e044a62ef7e12b6d1c2451915
tracer-ics2016 <tracer@njuics.org>
2016-10-21 17:35:03 +0800

    >  compile NEMU 151220135 xulijun Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.36-1+deb8u1 (2016-09-03) i686 GNU/Linux  17:35:03 up 18:01,  2 users,  load average: 0.02, 0.04, 0.05 8c1909fdcaf87a29eb78d176d113939292edffce

--- nemu/src/memory/cache-template.h
+++ nemu/src/memory/cache-template.h
@@ -79,7 +79,7 @@ int load_block(hwaddr_t addr, int set){
 	
 	}
 	Cache[set][i].valid=true;
-	Cache[set][i].tag=(addr/Size_of_Set)/Size_of_Cache_Block;
+	Cache[set][i].tag=(addr%Size_of_Set)/Size_of_Cache_Block;
 		
 	//	memcpy(Cache[set][i].block, addr, Size_of_Cache_Block);
 	uint32_t start= ( addr/Size_of_Cache_Block )*Size_of_Cache_Block;
