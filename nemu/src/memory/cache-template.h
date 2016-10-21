8727d62befc39dc3fcc933a4bf093955c93175f3
tracer-ics2016 <tracer@njuics.org>
2016-10-21 13:49:03 +0800

    >  compile NEMU 151220135 xulijun Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.36-1+deb8u1 (2016-09-03) i686 GNU/Linux  13:49:03 up 14:15,  3 users,  load average: 0.00, 0.01, 0.05 1852f0c65c7188ae5390f0ad72e46197b091e738

--- nemu/src/memory/cache-template.h
+++ nemu/src/memory/cache-template.h
@@ -101,6 +101,7 @@ void block_read(hwaddr_t addr,void *data){
 	{
 		//dram -->> cache;
 		way = load_block(addr,set);
+		Log("notFind");
 	}
 
 	memcpy(data, Cache[set][way].block , Size_of_Cache_Block);
