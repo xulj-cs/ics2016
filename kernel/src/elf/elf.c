203b43502ab18b69302476bf5bf86894525738ee
tracer-ics2016 <tracer@njuics.org>
2016-10-23 00:06:30 +0800

    >  compile kernel 151220135 xulijun Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.36-1+deb8u1 (2016-09-03) i686 GNU/Linux  00:06:30 up 2 days, 32 min,  2 users,  load average: 0.02, 0.04, 0.05 fbfd24feb295f513e6cefc23d112a261a212647b

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -62,7 +62,7 @@ uint32_t loader() {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
 	//		uint32_t hwaddr	= mm_malloc(ph->p_vaddr ,ph->p_memsz);
-//			mm_malloc(ph->p_vaddr ,ph->p_memsz);
+			mm_malloc(ph->p_vaddr ,ph->p_memsz);
 	//		Log("%x,%x",hwaddr,ph->p_vaddr);
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
