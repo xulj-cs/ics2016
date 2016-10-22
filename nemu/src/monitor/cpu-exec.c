3e2ccf0e5f712e4b31f7de716965e390a01dcdfa
tracer-ics2016 <tracer@njuics.org>
2016-10-23 01:16:09 +0800

    >  compile NEMU 151220135 xulijun Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.36-1+deb8u1 (2016-09-03) i686 GNU/Linux  01:16:09 up 2 days,  1:42,  2 users,  load average: 0.04, 0.03, 0.05 58edaef0682b35ff8753bbe7b311cd91675bf219

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -62,6 +62,8 @@ void cpu_exec(volatile uint32_t n) {
 		 * instruction decode, and the actual execution. */
 		int instr_len = exec(cpu.eip);
 		
+		if(cpu.eip==0x100d85)
+			Log("here2");
 		cpu.eip += instr_len;
 		if(cpu.eip==0x100d85)
 			Log("here");
@@ -73,8 +75,6 @@ void cpu_exec(volatile uint32_t n) {
 		if(n_temp < MAX_INSTR_TO_PRINT) {
 			printf("%s\n", asm_buf);
 		}
-		if(cpu.eip==0x100d85)
-		Log("here2");
 #endif
 
 		/* TODO: check watchpoints here. */
