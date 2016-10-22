bc634a8dc02eadabc899b36c1c16aa85489691cd
tracer-ics2016 <tracer@njuics.org>
2016-10-22 16:27:10 +0800

    >  compile NEMU 151220135 xulijun Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.36-1+deb8u1 (2016-09-03) i686 GNU/Linux  16:27:10 up 1 day, 16:53,  2 users,  load average: 0.24, 0.09, 0.06 64c06bdd03f7b86a4058058aa7dc3599a84ed49

--- nemu/src/cpu/exec/privileged/lgdt-template.h
+++ nemu/src/cpu/exec/privileged/lgdt-template.h
@@ -7,10 +7,11 @@ static void do_execute(){
 	
 	cpu.GDTR.Limit=swaddr_read(addr,2,DS);
 	cpu.GDTR.Base=(swaddr_t)swaddr_read((swaddr_t)((uint8_t *)addr+2),DATA_BYTE,DS);
-	
+	Log("%x,%x",swaddr_read(addr,4,0),swaddr_read(addr+4,4,0));	
 	print_asm_template1();
 }
 
 make_instr_helper(rm)
 
 #include "cpu/exec/template-end.h"
+
