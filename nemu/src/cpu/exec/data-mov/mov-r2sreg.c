88f7ee74c030ef076d01454093459a2c13371ea0
tracer-ics2016 <tracer@njuics.org>
2016-10-22 18:25:23 +0800

    >  compile NEMU 151220135 xulijun Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.36-1+deb8u1 (2016-09-03) i686 GNU/Linux  18:25:23 up 1 day, 18:51,  1 user,  load average: 0.16, 0.05, 0.06 b4f68f2957a1f066e300d93048b1ed0d386128d9

---
+++ nemu/src/cpu/exec/data-mov/mov-r2sreg.c
@@ -0,0 +1,31 @@
+#include "cpu/exec/helper.h"
+#include "cpu/decode/modrm.h"
+make_helper(mov_r2sreg){
+
+	ModR_M m;
+	m.val = instr_fetch(eip + 1, 1);
+	Assert(m.mod==3,"mod!=3");
+	
+	Assert(0<=m.reg&&m.reg<=3,"no this sreg");
+
+	cpu.sreg[m.reg].selector.val=reg_l(m.R_M);
+	
+	Assert(cpu.sreg[m.reg].selector.TI==0,"no LGTR");
+		
+	uint32_t base=cpu.GDTR.Base;
+	int max_index = ( cpu.GDTR.Limit+1 ) / sizeof(SegDesc) -1;
+    int index = cpu.sreg[m.reg].selector.INDEX;
+    if(index > max_index)
+        panic("Index out of range");
+						
+    uint8_t temp[8];
+    int j;
+    for(j=0;j<8;j++){
+        temp[j]=swaddr_read(base+index*8+j , 1 , CS);
+    }
+									
+    memcpy(&cpu.sreg[m.reg].descriptor,temp,8);
+
+	printf(str(mov) " %%%s,%%%s",regsl[m.R_M],sreg[m.reg]);
+	return 2;
+}
