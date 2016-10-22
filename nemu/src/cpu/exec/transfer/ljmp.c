5d479c5eeb73a09c1b505e36c6ff5789da4d9ed4
tracer-ics2016 <tracer@njuics.org>
2016-10-22 15:59:54 +0800

    >  compile NEMU 151220135 xulijun Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.36-1+deb8u1 (2016-09-03) i686 GNU/Linux  15:59:54 up 1 day, 16:26,  2 users,  load average: 0.02, 0.02, 0.05 881a59e81ec1d1452ce14e51da9c97d856347f11

---
+++ nemu/src/cpu/exec/transfer/ljmp.c
@@ -0,0 +1,23 @@
+#include "cpu/exec/helper.h"
+
+make_helper(ljmp){
+
+	uint32_t temp1=swaddr_read(eip+1,4,CS);
+	uint16_t temp2=swaddr_read(eip+1+4,2,CS);
+
+	cpu.CS.selector.val=temp2;
+
+	Assert(cpu.CS.selector.TI==0,"no LGTR");
+
+	int max_index = cpu.GDTR.Limit / sizeof(SegDesc) -1;
+	if(cpu.CS.selector.INDEX > max_index)
+		panic("Index out of range");
+
+	SegDesc *gdt_addr = (void *)cpu.GDTR.Base;
+
+	cpu.CS.descriptor=gdt_addr[cpu.CS.selector.INDEX];
+
+	cpu.eip=temp1;
+
+	return 0;
+}
