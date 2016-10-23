8bf8311e12aeb99970c5a3770dc77220653ed578
tracer-ics2016 <tracer@njuics.org>
2016-10-23 10:53:57 +0800

    >  compile NEMU 151220135 xulijun Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.36-1+deb8u1 (2016-09-03) i686 GNU/Linux  10:53:57 up 2 days, 11:20,  2 users,  load average: 0.05, 0.04, 0.05 61add9845dfdaa55b68813f945b996514156fc77

--- nemu/src/cpu/exec/interrupt/int.c
+++ nemu/src/cpu/exec/interrupt/int.c
@@ -10,12 +10,10 @@ make_helper(_int){
 	int len = decode_i_b( eip+1 );
 	
 	cpu.eip += len + 1;
-	print_asm_template1();
 	raise_intr((uint8_t)op_src->val);
 
-	print_asm_template1();
-	Log("reach here");
-	//a bug here
+	print_asm_template1();  // useless
+	panic("cannot reach here");
 	return 0;
 }
 
