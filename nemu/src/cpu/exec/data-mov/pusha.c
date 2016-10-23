618a63e8df27696cce51a0f37d97032c6032f0c0
tracer-ics2016 <tracer@njuics.org>
2016-10-23 09:04:24 +0800

    >  compile NEMU 151220135 xulijun Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.36-1+deb8u1 (2016-09-03) i686 GNU/Linux  09:04:24 up 2 days,  9:30,  2 users,  load average: 0.00, 0.01, 0.05 3174bbf87e47f372cac6e71f1d82e72fdfbc3c

--- nemu/src/cpu/exec/data-mov/pusha.c
+++ nemu/src/cpu/exec/data-mov/pusha.c
@@ -5,7 +5,7 @@ make_helper(pusha){
     int i;
     for(i=0;i<8;i++){
         cpu.esp -=4;
-        swaddr_write(cpu.esp, reg_l(i), 4 , SS);
+        swaddr_write(cpu.esp, 4, reg_l(i),  SS);
     }
 
 	print_asm(str(pusha));
