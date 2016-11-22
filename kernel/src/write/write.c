08f44701d8a6f3c9796427c3e5c9a136bb5984e7
tracer-ics2016 <tracer@njuics.org>
2016-11-22 11:08:21 +0800

    >  compile kernel 151220135 xulijun Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.36-1+deb8u1 (2016-09-03) i686 GNU/Linux  11:08:21 up  6:21,  2 users,  load average: 0.00, 0.04, 0.05 56823b2a3075b71a4b7b080e5662e39da68c553e

--- kernel/src/write/write.c
+++ kernel/src/write/write.c
@@ -1,9 +1,12 @@
 #include "common.h"
-
+void serial_printc(char);
 int write(int fd,const void *buf,size_t len){
 
 	if( (fd==1||fd==2)&&len!=0 ){
-		asm volatile (".byte 0xd6"::"a"(2),"c"(buf),"d"(len));
+//		asm volatile (".byte 0xd6"::"a"(2),"c"(buf),"d"(len));
+		int i;
+		for(i=0; i<len; i++)
+			serial_printc((char)*((uint8_t*)buf+i));
 		return len;
 	}
 	else
