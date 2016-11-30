724109b799bf538d3159d2d70cfb150ae762754c
tracer-ics2016 <tracer@njuics.org>
2016-11-30 16:26:58 +0800

    >  compile NEMU 151220135 xulijun Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.36-1+deb8u1 (2016-09-03) i686 GNU/Linux  16:26:58 up  9:11,  2 users,  load average: 1.38, 1.19, 1.16 e0dfcfae64268b0ad12a0ad264cade01f22f012a

--- nemu/src/cpu/exec/logic/bt-template.h
+++ nemu/src/cpu/exec/logic/bt-template.h
@@ -6,7 +6,7 @@ static void do_execute(){
 	
 	int offset = op_src->val % (8*DATA_BYTE);
 	//Log("%x,%x",op_src->val,op_dest->val);
-	cpu.CF = op_dest->val<<(31-offset)>>offset;
+	cpu.CF = op_dest->val<<(31-offset)>>31;
 	print_asm_template2();
 	
 }
